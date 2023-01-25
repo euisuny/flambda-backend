acaf77d5852e7593db02d399787767788a13bcd8
Irene Yoon <euisuny@cis.upenn.edu>
2023-01-25 11:43:43 +0000

    clean up

--- examples/fib.ml
+++ examples/fib.ml
@@ -22,31 +22,3 @@
 let rec fib n =
   if n < 2 then 1 else fib (n - 1) + fib (n - 2)
 ;;
-
-(*
-- In the branches of conditionals, sequences must be enclosed into
-  begin ... end. Hence, use
-    if cond then begin e1; e2 end else begin e3; e4 end
-
-- Some functions may fail to operate properly, in case of bad input.
-  In those situations, the function normally stop the computation, by
-  <EM>raising some exception</EM>. For instance int_of_string raises ``Failure
-  "int_of_string"'' when called with a string that cannot be
-  interpreted as a number.  You can check this situation, and continue
-  the computation appropriately, using the try ... with
-  ... construct. Hence, use
-    try expr with Exc -> e
-  to return e when the expression expr fails (raising exception Exc).
-*)
-
-(* let main () = *)
-(*    print_int (fib 3); ;; *)
-
-(*
-- !Sys.interactive tests if the program is called in an interactive
-   system or not.
-  If so, just do nothing (let the user call the fib function
-   directly).
-  Otherwise call the main procedure.
-*)
-(* if !Sys.interactive then () else main ();; *)
