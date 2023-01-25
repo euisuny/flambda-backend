bc0415b775c5ce38b90337f9dd834ae44f2364c7
Irene Yoon <euisuny@cis.upenn.edu>
2023-01-25 15:16:12 +0000

    slightly stabilized simple core_exp (without binding bureaucracy, wip translation

--- middle_end/flambda2/validate/validate.ml
+++ middle_end/flambda2/validate/validate.ml
@@ -1,4 +1,4 @@
-(* TODO: Renaming (do we need [With_delayed_renaming.t]? )*)
+open! Flambda
 
 module Result_continuation = struct
   type t = Continuation.t
@@ -8,6 +8,7 @@ module Exn_continuation = struct
   type t = Continuation.t
 end
 
+(* FIXME: Substitute [Simple.t] to [core_exp] *)
 module Apply = struct
   type t =
     { callee: Simple.t;
@@ -42,8 +43,12 @@ and let_expr = Bound_pattern.t * core_exp
 
 (* representing the recursive continuations, since this subsumes the non-recursive
    cont exprs *)
+(* [let k x = e1 in e2]
+   e1 = continuation
+   k = bound
+   body = e2 *)
 and let_cont_expr =
-  { continuation : Bound_pattern.t * core_exp;
+  { continuation : Continuation.t; (* FIXME : add code for continuation *)
     bound : Bound_pattern.t;
     body : core_exp; }
 
@@ -53,15 +58,20 @@ type eq = bool
 (* Simple program context *)
 type context = Bound_pattern.t list
 
-(* Closure context *)
+(* Closure context, keeping track of the continuation id's *)
 type closure_context = Continuation.t list
 
 let rec core_eq_ctx (ctx1:context) (ctx2:context) clo1 clo2 e1 e2 : eq =
   match e1, e2 with
   | Let (b1, e1), Let (b2, e2) ->
+    (* Add bound variables to context *)
     core_eq_ctx (b1::ctx1) (b2::ctx2) clo1 clo2 e1 e2
   | Let_cont {continuation = cont1; bound = bound1; body = body1},
     Let_cont {continuation = cont2; bound = bound2; body = body2} ->
+    (* [Continuation.t] is the names of the continuations, so this equality check
+       checks for [id] equality *)
+    Continuation.equal cont1 cont2 &&
+    (* Add bound variables to context, continuation id to closure context *)
     core_eq_ctx (bound1::ctx1) (bound2::ctx2) (cont1::clo1) (cont2::clo2) body1 body2
   | Apply { callee = callee1;
             continuation = cont1;
@@ -75,14 +85,45 @@ let rec core_eq_ctx (ctx1:context) (ctx2:context) clo1 clo2 e1 e2 : eq =
             call_kind = kind2 } -> false
   | Apply_cont _, Apply_cont _ ->
     false
-  | Switch _, Switch _->
+  | Switch _, Switch _ ->
     false
   | Invalid _, Invalid _ -> true
 
 let core_eq = core_eq_ctx [] [] [] []
 
-let flambda_unit_to_core = function
-  _ -> Invalid { message = "unimplemented" }
+let rec flambda_expr_to_core (e: expr) : core_exp =
+  let e = Expr.descr e in
+  match e with
+  | Flambda.Let e ->
+    Let_expr.pattern_match e ~f:(let_to_core)
+  | Flambda.Let_cont e ->
+    let_cont_to_core e
+  | Flambda.Apply t ->
+    Invalid { message = "unimplemented" }
+  | Flambda.Apply_cont t ->
+    Invalid { message = "unimplemented" }
+  | Flambda.Switch t ->
+    Invalid { message = "unimplemented" }
+  | Flambda.Invalid { message = t }->
+    Invalid { message = t }
+
+and let_to_core (var : Bound_pattern.t) ~body : core_exp =
+  Let (var, flambda_expr_to_core body)
+
+and let_cont_to_core (e : Let_cont_expr.t) : core_exp =
+  match e with
+  | Non_recursive
+      {handler = h; num_free_occurrences = n; is_applied_with_traps = b} ->
+    Invalid { message = "unimplemented" }
+  | Recursive r ->
+    Recursive_let_cont_handlers.pattern_match
+      r ~f:recursive_let_cont_handler_to_core
+
+and recursive_let_cont_handler_to_core ~invariant_params ~body t : core_exp =
+  Invalid { message = "unimplemented" }
+
+(* TODO *)
+let normalize = fun x -> x
 
 let simplify_result_to_core _e =
   Invalid { message = "unimplemented" }
@@ -91,8 +132,7 @@ let validate src tgt =
   let ret_cont = Continuation.create ~sort:Toplevel_return () in
   let exn_cont = Continuation.create () in
   let toplevel_my_region = Variable.create "toplevel_my_region" in
-  (* FIXME: Need to use renaming here? *)
-  let _mk_renaming u =
+  let mk_renaming u =
     let renaming = Renaming.empty in
     let renaming =
       Renaming.add_fresh_continuation renaming
@@ -111,6 +151,8 @@ let validate src tgt =
     in
     renaming
   in
-  let src_core = flambda_unit_to_core src in
+  let src_body = Expr.apply_renaming (Flambda_unit.body src) (mk_renaming src) in
+  (* let tgt_body = Expr.apply_renaming (Flambda_unit.body tgt) (mk_renaming tgt) in *)
+  let src_core = flambda_expr_to_core src_body in
   let tgt_core = simplify_result_to_core tgt in
   core_eq src_core tgt_core
--- middle_end/flambda2/validate/validate.mli
+++ middle_end/flambda2/validate/validate.mli
@@ -5,14 +5,23 @@
    translation), this validator does the following:
 
 
-   [Flambda_unit.t]   ------- Simplify.run --------> [simplify_result]
-          |                                                 |
-          |                                                 |
-     β - reduce                                          β - reduce
-          |                                                 |
-          |                                                 |
-          ↓                                                 ↓
-  [Flambda_core.t]            ≅[validate]            [Flambda_core.t]
+      [Flambda_unit.t]   ------- Simplify.run --------> [simplify_result]
+              |                                                 |
+              |                                                 |
+          translate                                        translate
+    (flambda_expr_to_core)                           (simplify_result_to_core)
+              |                                                 |
+              |                                                 |
+              ↓                                                 ↓
+          [core_exp]                                        [core_exp]
+              |                                                 |
+              |                                                 |
+          β - reduce                                        β - reduce
+          (normalize)                                       (normalize)
+              |                                                 |
+              |                                                 |
+              ↓                                                 ↓
+        [core_exp]              ≅[validate]                [core_exp]
 
    i.e. the validate function in this module performs the equivalence check
    between the source and target of the flambda2 simplification.
@@ -31,9 +40,11 @@ type eq
 
 val core_eq : core_exp -> core_exp -> eq
 
-val flambda_unit_to_core : Flambda_unit.t -> core_exp
+val flambda_expr_to_core : Flambda.expr -> core_exp
 
 val simplify_result_to_core : Simplify.simplify_result -> core_exp
 
+val normalize : core_exp -> core_exp
+
 val validate :
-  Flambda2_cmx.Flambda_cmx.loader -> Flambda_unit.t -> bool
+  Flambda_unit.t -> Simplify.simplify_result -> eq
