955c2aac813c460e24d3bec5e53e5230cbc115cf
Irene Yoon <euisuny@cis.upenn.edu>
2023-01-26 17:33:03 +0000

    add name_abstraction cruft to the core lang

--- middle_end/flambda2/validate/validate.ml
+++ middle_end/flambda2/validate/validate.ml
@@ -19,31 +19,37 @@ type core_exp =
    [fun x -> e] = let_abst
    [e1] = body *)
 and let_expr =
-  { let_abst : Bound_pattern.t * core_exp;
+  { let_abst : (Bound_pattern.t, core_exp) Name_abstraction.t;
     body : Flambda.named; }
 
 and let_cont_expr =
   (* Non-recursive case [let k x = e1 in e2]
 
-     [fun x -> e1] = continuation_handler
-     bound variable [k] = bound_cont
+     [fun x -> e1] = handler
+     bound variable [k] = Bound_continuation.t
      [e2] = body (has bound variable [k] in scope) *)
   | Non_recursive of
-    { continuation_handler : Bound_parameters.t * core_exp;
-      body : Bound_continuation.t * core_exp;}
+    { handler : continuation_handler;
+      body : (Bound_continuation.t, core_exp) Name_abstraction.t;}
 
   (* Recursive case, we have a set of (mutually recursive) continuations
      [let rec K x in e] where [K] is a map of continuations
      [x] is the set of invariant parameters
      bound variable [K] is in the scope of [e]
 
-     [x] = invariant_params
+     [x] = invariant_params (Bound_parameters.t)
      [K] = continuation_map
      [e] = body *)
   | Recursive of
-    { invariant_params : Bound_parameters.t;
-      continuation_map : (Bound_parameters.t * core_exp) Continuation.Map.t;
+    { continuation_map :
+        (Bound_parameters.t, continuation_handler_map) Name_abstraction.t;
       body : core_exp; }
+
+and continuation_handler =
+  (Bound_parameters.t, core_exp) Name_abstraction.t
+
+and continuation_handler_map =
+  continuation_handler Continuation.Map.t
 
 and apply_expr =
   { callee: core_exp;
@@ -60,49 +66,51 @@ and switch_expr =
   { scrutinee : core_exp;
     arms : core_exp Targetint_31_63.Map.t }
 
-(* The most naive equality type, a boolean *)
-type eq = bool
+let apply_renaming_core_exp = failwith "Unimplemented"
+let ids_for_export_core_exp = failwith "Unimplemented"
+let apply_renaming_cont_map = failwith "Unimplemented"
+let ids_for_export_cont_map = failwith "Unimplemented"
 
-(* Simple program context *)
-type context = Bound_pattern.t list
+module T0 = struct
+  type t = core_exp
+  let apply_renaming = apply_renaming_core_exp
+  let ids_for_export = ids_for_export_core_exp
+end
 
-(* Closure context, keeping track of the continuation id's *)
-type closure_context = Continuation.t list
+module ContMap = struct
+  type t = continuation_handler_map
+  let apply_renaming = apply_renaming_cont_map
+  let ids_for_export = ids_for_export_cont_map
+end
 
-let rec core_eq_ctx (ctx1:context) (ctx2:context) clo1 clo2 e1 e2 : eq =
-  (* match e1, e2 with
-   * | Let (b1, e1), Let (b2, e2) ->
-   *   (* Add bound variables to context *)
-   *   core_eq_ctx (b1::ctx1) (b2::ctx2) clo1 clo2 e1 e2
-   * | Let_cont _, Let_cont _ ->
-   *   (*   (Non_recursive { continuation_handler = (param1, exp1);
-   *    *                           body = (conbody1;}),
-   *    * Let_cont (Non_recursive { continuation_handler = (param2, exp2);
-   *    *                           letbound_contvar = bound_cont2;
-   *    *                           body = body2;}) -> *)
-   *   (* [Continuation.t] is the names of the continuations, so this equality check
-   *      checks for [id] equality *)
-   *   (* Continuation.equal cont1 cont2 && *)
-   *   (* Add bound variables to context, continuation id to closure context *)
-   *   (* core_eq_ctx (bound1::ctx1) (bound2::ctx2) (cont1::clo1) (cont2::clo2) body1 body2 *)
-   *   failwith "Unimplemented"
-   * | Apply { callee = callee1;
-   *           continuation = cont1;
-   *           exn_continuation = exn_cont1;
-   *           args = args1;
-   *           call_kind = kind1 },
-   *   Apply { callee = callee2;
-   *           continuation = cont2;
-   *           exn_continuation = exn_cont2;
-   *           args = args2;
-   *           call_kind = kind2 } -> failwith "Unimplemented"
-   * | Apply_cont _, Apply_cont _ -> failwith "Unimplemented"
-   * | Switch _, Switch _ -> failwith "Unimplemented"
-   * | Invalid _, Invalid _ -> failwith "Unimplemented" *)
-  failwith "Unimplemented"
+module Core_let = struct
+  module A = Name_abstraction.Make (Bound_pattern) (T0)
+  let create (bound_pattern : Bound_pattern.t) (defining_expr : named) body =
+    (match defining_expr, bound_pattern with
+    | Prim _, Singleton _
+    | Simple _, Singleton _
+    | Rec_info _, Singleton _
+    | Set_of_closures _, Set_of_closures _ -> ()
+    | _ , _ -> failwith "[Let.create] Mismatched bound pattern and defining expr");
+    Let { let_abst = A.create bound_pattern body; body = defining_expr }
+end
 
-let core_eq = core_eq_ctx [] [] [] []
+module Core_continuation_handler = struct
+  module A = Name_abstraction.Make (Bound_parameters) (T0)
+  let create = A.create
+end
 
+module Core_letrec_body = struct
+  module A = Name_abstraction.Make (Bound_continuation) (T0)
+  let create = A.create
+end
+
+module Core_continuation_map = struct
+  module A = Name_abstraction.Make (Bound_parameters) (ContMap)
+  let create = A.create
+end
+
+(* Translation *)
 let simple_to_core (v : Simple.t) : core_exp = Var v
 
 let rec flambda_expr_to_core (e: expr) : core_exp =
@@ -117,8 +125,7 @@ let rec flambda_expr_to_core (e: expr) : core_exp =
 
 and let_to_core (e : Let_expr.t) : core_exp =
   let (var, body) = Let_expr.pattern_match e ~f:(fun var ~body -> (var, body)) in
-  Let { let_abst = (var, flambda_expr_to_core body);
-        body = Let_expr.defining_expr e; }
+  Core_let.create var (Let_expr.defining_expr e) (flambda_expr_to_core body)
 
 and let_cont_to_core (e : Let_cont_expr.t) : core_exp =
   match e with
@@ -127,28 +134,27 @@ and let_cont_to_core (e : Let_cont_expr.t) : core_exp =
     let (contvar, scoped_body) =
       Non_recursive_let_cont_handler.pattern_match
         h ~f:(fun contvar ~body -> (contvar, body)) in
-    let cont_handler =
-      Non_recursive_let_cont_handler.handler h |> cont_handler_to_core in
     Let_cont (Non_recursive {
-      continuation_handler = cont_handler;
-      body = (contvar, flambda_expr_to_core scoped_body);})
+      handler =
+        Non_recursive_let_cont_handler.handler h |> cont_handler_to_core;
+      body = flambda_expr_to_core scoped_body |>
+        Core_letrec_body.create contvar;})
 
   | Recursive r ->
     let (params, body, handler) = Recursive_let_cont_handlers.pattern_match
       r ~f:(fun ~invariant_params ~body t -> (invariant_params, body, t))in
     Let_cont (Recursive {
-      invariant_params = params;
-      continuation_map = cont_handlers_to_core handler;
+      continuation_map =
+        Core_continuation_map.create params (cont_handlers_to_core handler);
       body = flambda_expr_to_core body;})
 
-and cont_handler_to_core (e : Continuation_handler.t) :
-    Bound_parameters.t * core_exp =
+and cont_handler_to_core (e : Continuation_handler.t) : continuation_handler =
   let (var, handler) =
     Continuation_handler.pattern_match e ~f:(fun var ~handler -> (var, handler)) in
-  (var, flambda_expr_to_core handler)
+  Core_continuation_handler.create var (flambda_expr_to_core handler)
 
 and cont_handlers_to_core (e : Continuation_handlers.t) :
-  (Bound_parameters.t * core_exp) Continuation.Map.t =
+  continuation_handler Continuation.Map.t =
   let e : Continuation_handler.t Continuation.Map.t =
     Continuation_handlers.to_map e in
   Continuation.Map.map cont_handler_to_core e
@@ -172,8 +178,36 @@ and switch_to_core (e : Switch.t) : core_exp =
     scrutinee = Switch_expr.scrutinee e |> simple_to_core;
     arms = Switch_expr.arms e |> Targetint_31_63.Map.map apply_cont_to_core;}
 
-(* TODO *)
-let rec normalize = fun x -> x
+(* The most naive equality type, a boolean *)
+type eq = bool
+
+(* Simple program context *)
+module Env = struct
+  type t = Bound_pattern.t list (* FIXME *)
+  let create () = []
+end
+
+let rec normalize (e:core_exp) : core_exp =
+  match e with
+  | Var _ -> e
+  | Let e -> failwith "Unimplemented"
+  | Let_cont _ -> failwith "Unimplemented"
+  | Apply _ -> failwith "Unimplemented"
+  | Apply_cont _ -> failwith "Unimplemented"
+  | Switch _ -> failwith "Unimplemented"
+  | Invalid _ -> failwith "Unimplemented"
+
+(* Equality between two programs given a context *)
+let rec core_eq_ctx (e:Env.t) e1 e2 : eq =
+  match e1, e2 with
+  | Let _, Let _ -> failwith "Unimplemented"
+  | Let_cont _, Let_cont _ -> failwith "Unimplemented"
+  | Apply _, Apply _ -> failwith "Unimplemented"
+  | Apply_cont _, Apply_cont _ -> failwith "Unimplemented"
+  | Switch _, Switch _ -> failwith "Unimplemented"
+  | Invalid _, Invalid _ -> failwith "Unimplemented"
+
+let core_eq = Env.create () |> core_eq_ctx
 
 let simulation_relation src tgt =
   let {Simplify.unit = tgt; exported_offsets; cmx; all_code} = tgt in
