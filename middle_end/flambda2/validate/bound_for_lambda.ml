cc41a7d670b338f58f55d60cfe689cc5728dfa28
Irene Yoon <euisuny@cis.upenn.edu>
2023-02-28 11:27:31 +0000

    named lambdas for recursive code blocks

--- middle_end/flambda2/validate/bound_for_lambda.ml
+++ middle_end/flambda2/validate/bound_for_lambda.ml
@@ -1,23 +1,29 @@
 type t =
-  { return_continuation: Bound_continuation.t;
+  { name: Code_id.t;
+    return_continuation: Bound_continuation.t;
     exn_continuation: Bound_continuation.t;
     params: Bound_parameters.t }
 
-let create ~return_continuation ~exn_continuation ~params =
+let create ~name ~return_continuation ~exn_continuation ~params =
   Bound_parameters.check_no_duplicates params;
-  { return_continuation;
+  { name;
+    return_continuation;
     exn_continuation;
     params;
   }
 
 let free_names
-      { return_continuation;
+      { name;
+        return_continuation;
         exn_continuation;
         params;
       } =
   (* See [bound_continuations.ml] for why [add_traps] is [true]. *)
   let free_names =
-    Name_occurrences.add_continuation Name_occurrences.empty return_continuation
+    Name_occurrences.add_code_id Name_occurrences.empty name Name_mode.normal
+  in
+  let free_names =
+    Name_occurrences.add_continuation free_names return_continuation
       ~has_traps:true
   in
   let free_names =
@@ -30,9 +36,13 @@ let free_names
   free_names
 
 let apply_renaming
-      { return_continuation;
+      { name;
+        return_continuation;
         exn_continuation;
         params} renaming =
+  let name =
+    Renaming.apply_code_id renaming name
+  in
   let return_continuation =
     Renaming.apply_continuation renaming return_continuation
   in
@@ -40,54 +50,69 @@ let apply_renaming
     Renaming.apply_continuation renaming exn_continuation
   in
   let params = Bound_parameters.apply_renaming params renaming in
-  { return_continuation;
+  { name;
+    return_continuation;
     exn_continuation;
     params;
   }
 
 let ids_for_export
-      { return_continuation;
+      { name;
+        return_continuation;
         exn_continuation;
         params;
       } =
   let ids =
-    Ids_for_export.add_continuation Ids_for_export.empty return_continuation
+    Ids_for_export.add_code_id Ids_for_export.empty name
+  in
+  let ids =
+    Ids_for_export.add_continuation ids return_continuation
   in
   let ids = Ids_for_export.add_continuation ids exn_continuation in
   Ids_for_export.union ids (Bound_parameters.ids_for_export params)
 
 let[@ocamlformat "disable"] print ppf
-      { return_continuation; exn_continuation; params; } =
+     { name; return_continuation; exn_continuation; params; } =
   Format.fprintf ppf "@[<hov 1>(\
+                      @[<hov 1>(name@ %a)@]@ \
                       @[<hov 1>(return_continuation@ %a)@]@ \
                       @[<hov 1>(exn_continuation@ %a)@]@ \
                       @[<hov 1>(params@ %a)@])@]"
+    Code_id.print name
     Continuation.print return_continuation
     Continuation.print exn_continuation
     Bound_parameters.print params
 
 let rename
-      { return_continuation;
+      { name;
+        return_continuation;
         exn_continuation;
         params;
       } =
-  { return_continuation = Continuation.rename return_continuation;
+  { name = Code_id.rename name;
+    return_continuation = Continuation.rename return_continuation;
     exn_continuation = Continuation.rename exn_continuation;
     params = Bound_parameters.rename params;
   }
 
 let renaming
-    { return_continuation = return_continuation1;
+    { name = name1;
+      return_continuation = return_continuation1;
       exn_continuation = exn_continuation1;
       params = params1;
     }
     ~guaranteed_fresh:
-      { return_continuation = return_continuation2;
-        exn_continuation = exn_continuation2;
-        params = params2;
-      } =
+    { name = name2;
+      return_continuation = return_continuation2;
+      exn_continuation = exn_continuation2;
+      params = params2;
+    } =
   let renaming =
-    Renaming.add_fresh_continuation Renaming.empty return_continuation1
+    Renaming.add_fresh_code_id Renaming.empty name1
+      ~guaranteed_fresh:name2
+  in
+  let renaming =
+    Renaming.add_fresh_continuation renaming return_continuation1
       ~guaranteed_fresh:return_continuation2
   in
   let renaming =
--- middle_end/flambda2/validate/bound_for_lambda.mli
+++ middle_end/flambda2/validate/bound_for_lambda.mli
@@ -1,9 +1,11 @@
 type t =
-  { return_continuation: Bound_continuation.t;
+  { name: Code_id.t;
+    return_continuation: Bound_continuation.t;
     exn_continuation: Bound_continuation.t;
     params: Bound_parameters.t }
 
 val create :
+  name:Code_id.t ->
   return_continuation:Continuation.t ->
   exn_continuation:Continuation.t ->
   params:Bound_parameters.t ->
